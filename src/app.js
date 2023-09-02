30115a1d9aac23a3dac7597a05cb24df016cba32
amitsingh6391 <amitsingh506142@gmail.com>
2023-09-02 23:58:13 +0530

    change port:

--- src/app.js
+++ src/app.js
@@ -36,7 +36,7 @@ app.use('/projects', projectRoutes({ projectController }));
 module.exports = app;
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
--- src/config/database.js
+++ src/config/database.js
@@ -6,12 +6,12 @@ require('dotenv').config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: '',
+    password: process.env.PASSWORD || '',
     database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    port: 4306
+    port: process.env.DB_PORT || 4306
 });
 
 
