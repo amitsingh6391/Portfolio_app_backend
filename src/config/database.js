bc392458fd0f5c60422c35bbcc8c832f1e38e7d7
amitsingh6391 <amitsingh506142@gmail.com>
2023-09-03 11:31:40 +0530

    change port

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,8 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
+    host: '127.0.0.1',
+    // process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.PASSWORD || '',
     database: process.env.DB_NAME,
